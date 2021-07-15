6908656c3c024f3a59a090a8d09889eeb1be379f
Kevin Gammenthaler <kevin.gammenthaler@gmail.com>
2021-07-15 11:10:44 -0500

    added styling to home page

--- dist/scripts.bcf3243b.js
+++ dist/scripts.bcf3243b.js
@@ -18547,7 +18547,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60646" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "56051" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
