1bdaeba463f7a8e9f15b838db6addeeba4f919a2
Kevin Gammenthaler <kevin.gammenthaler@gmail.com>
2021-07-21 01:25:25 -0500

    added eleventy functionality and njk templates

--- .eleventy.js
+++ .eleventy.js
@@ -1,42 +1,14 @@
-// module.exports = function (eleventyConfig) {
-//   eleventyConfig.addPassthroughCopy("./_public/css/**/*.css");
-//   eleventyConfig.addPassthroughCopy("./src/images");
-//   eleventyConfig.addPassthroughCopy("./src/fonts");
-//   eleventyConfig.addPassthroughCopy("./src/scripts");
-//   eleventyConfig.addPlugin(require("eleventy-load"), {
-//     rules: [
-//       {
-//         test: /\.html$/,
-//         loaders: [
-//           {
-//             loader: require("eleventy-load-html"),
-//           },
-//         ],
-//       },
-//       {
-//         test: /\.scss$/,
-//         loaders: [
-//           {
-//             loader: require("eleventy-load-sass"),
-//           },
-//           {
-//             loader: require("eleventy-load-css"),
-//           },
-//           {
-//             loader: require("eleventy-load-file"),
-//             options: {
-//               name: "[hash].css",
-//             },
-//           },
-//         ],
-//       },
-//     ],
-//   });
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("src/css");
+  eleventyConfig.addPassthroughCopy("src/images");
+  eleventyConfig.addPassthroughCopy("src/fonts");
+  eleventyConfig.addPassthroughCopy("src/scripts");
+  eleventyConfig.addPassthroughCopy("src/lottie.js");
 
-//   return {
-//     dir: {
-//       input: "src",
-//       output: "_public",
-//     },
-//   };
-// };
+  return {
+    dir: {
+      input: "src",
+      output: "_public",
+    },
+  };
+};
