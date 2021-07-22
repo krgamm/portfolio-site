2b5c245e9a4649c8aad7482732a625c02315f132
Kevin Gammenthaler <kevin.gammenthaler@gmail.com>
2021-07-21 22:05:08 -0500

    updated template for projects

--- .eleventy.js
+++ .eleventy.js
@@ -1,14 +1,14 @@
 module.exports = function (eleventyConfig) {
-  eleventyConfig.addPassthroughCopy("src/css");
-  eleventyConfig.addPassthroughCopy("src/images");
-  eleventyConfig.addPassthroughCopy("src/fonts");
-  eleventyConfig.addPassthroughCopy("src/scripts");
-  eleventyConfig.addPassthroughCopy("src/lottie.js");
+  eleventyConfig.addPassthroughCopy("./src/css");
+  eleventyConfig.addPassthroughCopy("./src/images");
+  eleventyConfig.addPassthroughCopy("./src/fonts");
+  eleventyConfig.addPassthroughCopy("./src/scripts");
+  eleventyConfig.addPassthroughCopy("./src/lottie.js");
 
   return {
     dir: {
       input: "src",
-      output: "_public",
+      output: "public",
     },
   };
 };
