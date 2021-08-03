5c451bb5e8072b34e6c2979318d3da95cd837d55
Kevin Gammenthaler <kevin.gammenthaler@gmail.com>
2021-08-02 21:17:16 -0500

    added functionality and project formatting

--- frontend/src/scripts/index.js
+++ frontend/src/scripts/index.js
@@ -62,6 +62,28 @@ navToggle.addEventListener("click", () => {
 });
 
 //----------------------------------------
+// Hide Nav on scroll
+//----------------------------------------
+let prevScrollpos = window.pageYOffset;
+
+document.addEventListener("mousemove", (e) => {
+  let mouseY = e.clientY;
+  if (mouseY <= 110) {
+    document.querySelector("nav").style.top = "0";
+  }
+});
+
+window.onscroll = function () {
+  let currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.querySelector("nav").style.top = "0";
+  } else {
+    document.querySelector("nav").style.top = "-120px";
+  }
+  prevScrollpos = currentScrollPos;
+};
+
+//----------------------------------------
 // Carousel Interaction
 //----------------------------------------
 
